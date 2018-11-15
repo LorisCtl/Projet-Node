879961cec10fab947a007e3342ddfde7c63faa44
Loris <loris.catallo@ynov.com>
2018-11-15 11:22:32 +0100

    mise en place de la Q2

--- ask.js
+++ ask.js
@@ -41,13 +41,55 @@ setTimeout(() => {
 }, 3000);
 
 setTimeout(() => {
-  console.log('Question suivante !');
+  console.log('Question suivante ! \n');
 }, 4000);
 
 
+q2()
+
+
+function q2(){
+  setTimeout(() => {
+    console.log('Quel âge a la Reine d\'Angleterre ? \n');
+    console.log('A) 94 ans \n')
+    console.log('B) 250 ans mais elle est très bien conservée \n')
+    console.log('C) 90 ans \n')
+    console.log('D) 92 ans\n')
+  }, 7000);
+
+
+  var prompt = require("prompt");
+  prompt.start();
+  /*console.log("Veuillez Répondre a la question");*/
+  prompt.get(["reponse"]), function(err, res){
+console.log('Pomme de Terre ')
+
+  var expre = 'A';
+  switch (res.reponse) {
+    case 'D':
+      console.log('Bravo ! Vous gagnez un point !');
+     score = score + 1;
+      break;
+    case 'A':
+    case 'C':
+    case 'B':
+      console.log('Non, désolé, La bonne réponse était : 92 ans');
+      break;
+    }
+    console.log("Votre score est : ".concat(score, "."));
+  }
 
 
 
+
+
+
+
+
+
+
+
+}
 
 });
 function start(){
