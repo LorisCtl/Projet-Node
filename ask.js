32cceb089db6efca0769152a92582934e027a7cd
Loris <loris.catallo@ynov.com>
2018-11-18 18:36:12 +0100

    Si avec ça jai la moyenne

--- ask.js
+++ ask.js
@@ -26,7 +26,7 @@ switch (result.rep) {
     console.log('----- REGLES DU JEU ----- \n ');
     console.log('Nous allons vous poser une série de questions sur de la culture Générale.\nUne bonne réponse vous donne 1 point, une mauvaise vous enlèves 3 cheveux \nA la fin,votre scrore est affiché et vous pourrez aller frimer si vous avez été bon. \n ')
     console.log('Pour Répondre a une question, il vous suffit de taper la lettre qui correspond, vous allez voir c\'est pas compliqué !')
-    console.log('LA partie commence dans 15 secondes ! \n')
+    console.log('La partie commence dans 15 secondes ! \n')
     setTimeout(() => {
       function2()
     }, 15000);
@@ -42,7 +42,7 @@ function2()
 
 
 function function2() {
-  console.log('----------- QUESTION 1 ---------- \n')
+  console.log('----------- QUESTION I ---------- \n')
 
   console.log('En quelle année  le TITANIC a-t-il sombré ?\n')
   console.log('A) 1912 \n')
@@ -71,19 +71,77 @@ function function2() {
     console.log('Malheuresement, c\'est une mauvaise réponse !. \nLe TITANIC a sombré dans la nuit du 14 au 15 Avril 1912. Comment ça on s\'en fout ?  \n');
     console.log('Votre Score est maintenant de ' +score+ 'point.' )
     }, 2000);
-function3()
+function3(function4)
       break;
   }
    });
 
 
-function function3(){
-  console.log('On Passe a la suite ! ')
+function function3(callback){
+  console.log('On Passe a la suite !  ')
+  setTimeout(() => {
+    console.log('---------- QUESTION II ----------  \n')
+    console.log('Lequel de ces joueurs est le meilleur buteur de l\'Equipe De France de Football ?\n')
+    console.log('A)  Michel Platini\n')
+    console.log('B)  Olivier Giroud\n')
+    console.log('C)  Zinedine Zidane\n')
+    console.log('D)  Thierry Henry\n')
+  }, 2000);
 
+  var prompt = require('prompt');
+
+   prompt.start();
+
+   prompt.get(['rep',], function (err, result) {
+
+
+  switch (result.rep) {
+    case 'D':
+      console.log(' Bien joué ! Vous gagnez 1 point ! \n ');
+      score++
+      console.log('Votre Score est maintenant de ' +score+ 'point.' )
+      function4()
+      break;
+    case 'B':
+    case 'C':
+    case 'A':
+    setTimeout(() => {
+    console.log('Malheuresement, c\'est une mauvaise réponse !. \nLe meilleur buteur est Thierry Henry avec 51 buts !   \n');
+    console.log('Votre Score est maintenant de ' +score+ 'point(s). \n' )
+    }, 2000);
+function4(function5)
+callback
+      break;
+  }
+
+});
+}
+
+function function4(){
+  setTimeout(() => {
+console.log('Ok, on continue !Et voici la dernière question  !\n')
+}, 2000);
+function5()
+}
+
+function function5(){
+
+  setTimeout(() => {
+    console.log('---------- QUESTION III ----------  \n')
+    console.log('Laquelle de ces villes est la capitale de l\'Equateur ?\n')
+    console.log('A)  Rito\n')
+    console.log('B)  Pepito\n')
+    console.log('C)  Quito\n')
+    console.log('D)  Mito\n')
+      }, 2000);
+}
 }
 
 
-}
+
+
+
+
 
 
 function start() {
